719f12457ffeaf94b1619c1e8dd68083bfa1a71e
JulianaCelis <juliana0celis@email.com>
2023-10-01 18:27:42 -0500

    arreglados problemas de merging

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
 import styles from "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./Views/Landing/landing";
 import Home from "./Views/Home/home";
 import EventDetail from "./Views/Detail/EventDetail";
+import FormLogin from './Components/FormLogin/FormLogin';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route index element={<Landing/>} />
-        <Route exact path ="/home" element ={<Home/>}/>
-        <Route path="/detail/:id" />
-        <Route path="/checkout" />
-        <Route path="/login" element={<FormLogin/>} />
-      </Routes>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<EventDetail />} />
+          <Route path="/checkout" />
+          <Route path="/login" element={<FormLogin />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/Views/Home/home.jsx
+++ src/Views/Home/home.jsx
@@ -1,23 +1,29 @@
-import React, {useEffect, useRef, useState} from "react";
-import { useNavigate } from "react-router-dom";
-import style from "./home.module.css"
-import NavBar from "../../Components/NavBar/NavBar"
-import Cards from '../../Components/Cards/Cards'
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Importa Routes y Route
+import style from "./home.module.css";
+import NavBar from "../../Components/NavBar/NavBar";
+import Cards from "../../Components/Cards/Cards";
 import Sidebar from "../../Components/SideBar/SideBar";
+import EventDetail from "../Detail/EventDetail"; // Asegúrate de importar desde la ubicación correcta
+import Header from "../Header/Header"
 
-function Home (){
-const navigate = useNavigate;
-
-
-
-return (
- <div className={style.container}>
-    <NavBar/>
-    <Sidebar/>
-    <Cards/>
- </div>
-)
+function Home() {
+  return (
+    <div className={style.container}>
+      <div className={style.navbar}>
+        <NavBar />
+      </div>
+      <div className={style.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={style.main}>
+        <Cards />
+      </div>
+      <Routes>
+        <Route path="/detail/:id" element={<EventDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Home;
-
