504933af0710943b1810f2c31b51aef9b6a42772
Dario Sosa <xdsb.tstx@gmail.com>
2023-10-03 14:17:22 -0600

    home2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-
 import styles from "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./Views/Landing/landing";
 import Home from "./Views/Home/home";
-import FormLogin from "./Components/FormLogin/FormLogin";
+import EventDetail from "./Views/Detail/EventDetail";
+import FormLogin from './Components/FormLogin/FormLogin';
 import Cards from "./Components/Cards/Cards";
-
+import About from "./Views/About/About";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route index element={<Landing/>} />
-        <Route exact path ="/home" element ={<Home/>}/>
-        {/* <Route path="/detail/:id" element={<EventDetail/>} /> */}
-        <Route path="/checkout" />
-        <Route path="/login" element={<FormLogin/>} />
-        <Route path="/todos" element ={<Cards/>}/>
-      </Routes>
+        <Routes>
+          <Route index element={<Landing />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route path="/detail/:id" element={<EventDetail />} />
+          <Route path="/checkout" />
+          <Route path="/login" element={<FormLogin />} />
+          <Route path="/about" element={<About/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
--- src/Views/Home/home.jsx
+++ src/Views/Home/home.jsx
@@ -1,42 +1,29 @@
-import React, {useEffect, useRef, useState} from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import style from "./home.module.css"
+import React from "react";
+import { Routes, Route } from "react-router-dom"; // Importa Routes y Route
+import style from "./home.module.css";
 import NavBar from "../../Components/NavBar/NavBar";
-import Card from "../../Components/Card/Card";
 import Cards from "../../Components/Cards/Cards";
-import FormLogin from "../../Components/FormLogin/FormLogin";
+import Sidebar from "../../Components/SideBar/SideBar";
+import EventDetail from "../Detail/EventDetail"; // Asegúrate de importar desde la ubicación correcta
+import Header from "../Header/Header"
 
-function Home (){
-const navigate = useNavigate;
-
-
-
-return (
- <body>
+function Home() {
+  return (
     <div className={style.container}>
-    <NavBar/>
-    <div className={style.card}>
-        <Link to={`/detail/`}  >
-         <Card/>
-        
-        </Link>
-        <Card/>
-        <Card/>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li> <li> <li>  <Card/></li></li> </li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>
-        <li>  <Card/></li>  
-              </div>
- </div>
-</body>
-)
+      <div className={style.navbar}>
+        <NavBar />
+      </div>
+      <div className={style.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={style.main}>
+        <Cards />
+      </div>
+      <Routes>
+        <Route path="/detail/:id" element={<EventDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default Home;
