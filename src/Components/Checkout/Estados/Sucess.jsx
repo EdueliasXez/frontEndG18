89e77f87b1035e5a4435a6ca275ec9a09d4e1abd
JulianaCelis <juliana0celis@email.com>
2023-10-17 09:57:43 -0500

    manejo de sucees en checkout, y base de datos

--- src/Components/Checkout/Estados/Sucess.jsx
+++ src/Components/Checkout/Estados/Sucess.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./SucessPage.css"; 
+import { connect } from "react-redux";
+import axios from 'axios'; // Importa Axios
+import { getUserProfileFromToken } from "../../../Redux/actions/auth_actions"; 
 
-const SucessPage = () => {
+import "./SucessPage.css";
+
+const SucessPage = (props) => {
+  const { checkoutData, isAuthenticated } = props;
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      getUserProfileFromToken() 
+        .then((data) => {
+          setUserId(data._id);
+          const cartItems = checkoutData.cartItems;
+          if (cartItems) {
+            const requestData = {
+              cartItems: cartItems, 
+              userId: data._id
+            };
+            axios.post('checkout/success', requestData)
+              .then(response => {
+              })
+              .catch(error => {
+
+              });
+          }
+        })
+        .catch((error) => {
+          console.error("Error al obtener el userId:", error);
+        });
+    }
+  }, [checkoutData, isAuthenticated]);
+
   return (
     <div className="thank-you-container">
       <h2>¡Gracias por confiar en nosotros! 😄</h2>
-      <p>Tu compra fue hecha con exito, te invitamos a compartir tu experiencia en el evento.</p>
+      <p>Tu compra fue hecha con éxito, te invitamos a compartir tu experiencia en el evento.</p>
       <Link to="/home">
         <button className="back-button">Volver al Inicio</button>
       </Link>
@@ -14,4 +46,11 @@ const SucessPage = () => {
   );
 };
 
-export default SucessPage;
+const mapStateToProps = (state) => {
+  return {
+    checkoutData: state.cart.checkoutData,
+    isAuthenticated: state.login.isAuthenticated,
+  };
+};
+
+export default connect(mapStateToProps)(SucessPage);
