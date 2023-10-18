508e1724931f2ab550afd006696a9d8931bf05e5
kayitaC2024 <kayita_y@hotmail.com>
2023-10-18 01:26:06 -0500

    los items en card y detail

--- src/Components/SideBar/Filters/LocationFilter.jsx
+++ src/Components/SideBar/Filters/LocationFilter.jsx
@@ -19,22 +19,28 @@ const LocationFilter = ({ eventLocations, filterEventsByLocation }) => {
   return (
     <div>
       <h3>Filtrar por localización</h3>
-      <select value={selectedCountry} onChange={handleCountryChange}>
-        <option value="">Todos los países</option>
-        {eventLocations.countries.map((country) => (
-          <option key={country} value={country}>
-            {country}
-          </option>
-        ))}
-      </select>
-      <select value={selectedCity} onChange={handleCityChange}>
-        <option value="">Todas las ciudades</option>
-        {eventLocations.cities.map((city) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
-        ))}
-      </select>
+      {eventLocations ? (
+      <div>
+        <select value={selectedCountry} onChange={handleCountryChange}>
+          <option value="">Todos los países</option>
+          {eventLocations.countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        <select value={selectedCity} onChange={handleCityChange}>
+          <option value="">Todas las ciudades</option>
+          {eventLocations.cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+      </div>
+    ) : (
+      <p>Cargando datos...</p>
+    )}
     </div>
   );
 };
@@ -48,3 +54,59 @@ const mapDispatchToProps = {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(LocationFilter);
+
+
+
+
+////////////////////////
+
+// import React, { useState } from 'react'; original
+// import { connect } from 'react-redux';
+// import { filterEventsByLocation } from '../../../Redux/actions/events_actions';
+
+// const LocationFilter = ({ eventLocations, filterEventsByLocation }) => {
+//   const [selectedCountry, setSelectedCountry] = useState('');
+//   const [selectedCity, setSelectedCity] = useState('');
+
+//   const handleCountryChange = (event) => {
+//     setSelectedCountry(event.target.value);
+//     filterEventsByLocation(event.target.value, selectedCity);
+//   };
+
+//   const handleCityChange = (event) => {
+//     setSelectedCity(event.target.value);
+//     filterEventsByLocation(selectedCountry, event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <h3>Filtrar por localización</h3>
+//       <select value={selectedCountry} onChange={handleCountryChange}>
+//         <option value="">Todos los países</option>
+//         {eventLocations.countries.map((country) => (
+//           <option key={country} value={country}>
+//             {country}
+//           </option>
+//         ))}
+//       </select>
+//       <select value={selectedCity} onChange={handleCityChange}>
+//         <option value="">Todas las ciudades</option>
+//         {eventLocations.cities.map((city) => (
+//           <option key={city} value={city}>
+//             {city}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = (state) => ({
+//   eventLocations: state.events.eventLocations,
+// });
+
+// const mapDispatchToProps = {
+//   filterEventsByLocation,
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(LocationFilter);
