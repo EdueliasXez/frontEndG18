5ae3eb6a8a497fdfaddd3e9204b74105bb486897
Dario Sosa <xdsb.tstx@gmail.com>
2023-10-19 00:48:53 -0600

    serv

--- src/Views/ServiceProvider/Service.jsx
+++ src/Views/ServiceProvider/Service.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getEvents } from '../../Redux/actions/events_actions';
-import style from "./Service.module.css"
+import style from './Service.module.css';
 import { Link } from 'react-router-dom';
+
 const ServiceProviderEvents = () => {
   const dispatch = useDispatch();
-  const { events, loading, error } = useSelector((state) => state.events); 
+  const { events, loading, error } = useSelector((state) => state.events);
   useEffect(() => {
     dispatch(getEvents());
   }, [dispatch]);
@@ -19,34 +20,28 @@ const ServiceProviderEvents = () => {
   }
 
   // Filtrar eventos por serviceProviderId
-  const serviceProviderId = '636c69636b795469636b6574'; 
+  const serviceProviderId = '636c69636b795469636b6574';
   const filteredEvents = events.filter((event) => {
     return event.serviceProviderId === serviceProviderId;
   });
 
   return (
     <div>
-      <h1 className={style.h1} >  Eventos organizados por : {serviceProviderId}</h1>
-      <ul>
-        <div className={style.content}>
+      <h1 className={style.h1}>Eventos organizados por : {serviceProviderId}</h1>
+      <div className={style.scrollableContent}>
+        <ul>
           {filteredEvents.map((event) => (
-          <li key={event._id}>
-            <h3 className={style.font} >{event.title}</h3>
-            <p className={style.txt}> ID: {event._id}</p>
-            
-            <ul/>
-            <Link to={`/put/${event._id}`}>
-              <button className={style.btn}>
-                MODIFICAR
-              </button>
-            
-          </Link>
-           
-                  </li>
-        ))}
-      
-        </div>  
-      </ul>
+            <li key={event._id}>
+              <h3 className={style.font}>{event.title}</h3>
+              <p className={style.txt}> ID: {event._id}</p>
+              <ul />
+              <Link to={`/put/${event._id}`}>
+                <button className={style.btn}>MODIFICAR</button>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
