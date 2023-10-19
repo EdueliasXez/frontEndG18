4b072e4f6b9f1c56c7dc19a827d3f6d0f5575060
Mateoch14 <89880893+Mateoch14@users.noreply.github.com>
2023-10-19 02:56:08 -0300

    revisar rutas

--- src/Redux/actions/userActions.js
+++ src/Redux/actions/userActions.js
@@ -1,6 +1,14 @@
-export const updateUser = (userData) => {
-    return {
-      type: 'UPDATE_USER',
-      payload: userData,
-    };
+// userActions.js
+export const editUser = (user) => {
+  return {
+    type: 'EDIT_USER',
+    payload: user,
   };
+};
+
+export const updateUser = (user) => {
+  return {
+    type: 'UPDATE_USER',
+    payload: user,
+  };
+};
